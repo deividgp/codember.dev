b2b21f7074f44db4447dc932a46f82471c5fb99d
deividgp <dayd4@outlook.com>
2022-11-22 23:34:29 +0100

    Add challenge04

---
+++ challenge04/index.js
@@ -0,0 +1,20 @@
+const passwords = [];
+
+for (let index = 11098; index <= 98123; index++) {
+  const number = index.toString();
+
+  if ((number.match(/5/g) || []).length < 2) continue;
+
+  for (let index2 = 0; index2 + 1 < number.length; index2++) {
+    if (number[index2] > number[index2 + 1]) break;
+
+    if (index2 == 3) passwords.push(number);
+  }
+}
+
+console.log(
+  "NUMBER OF PASSWORDS: " +
+    passwords.length +
+    " PASSWORD INDEX 55: " +
+    passwords[55]
+);
